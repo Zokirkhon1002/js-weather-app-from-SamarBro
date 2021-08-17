fa1f5811befe89c8b4be25194dbae88f9dcadb2c
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2021-08-18 01:30:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const api = {
+    key: '4e7b7cae3f92ddcb1a05b8c5eec7c40b',
+    baseurl: 'https://api.openweathermap.org/data/2.5/',
+};
+
+const searchBox = document.querySelector('.search-box');
+
+searchBox.addEventListener('keypress',setQuery)
+
+function setQuery(e) {
+    if(e.keyCode == 13) {
+        getResults(searchBox.value)
+        console.log(searchBox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    let city = document.querySelector(".location .city")
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerHTML = dateBuilder(now);
+
+
+    let temp = document.querySelector(".temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let weatherEl = document.querySelector(".weather");
+    weatherEl.innerHTML = weather.weather[0].main;
+
+
+    let highlow = document.querySelector(".hi-low");
+    highlow.innerHTML = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+}
+
+
+function dateBuilder(s) {
+    let months = ['Yanvar', "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentyabr", "October", "November","December"];
+    let days = ["Yakshanba", "Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Shanba"];
+    let day = days[s.getDay()];
+    let date = s.getDate();
+    let month = months[s.getMonth()];
+    let year = s.getFullYear();
+    
+    return `${day} ${date} ${month} ${year}`
+}
